6fe5a663cc5f44e34816fcfa45f863e19e5d556c
Pingoin <p.drente@gmx.de>
2023-11-04 13:21:44 +0100

    feat: migrated to sycamore 0.9 and svg-logo

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,22 @@ use crate::utils::fetch;
 mod utils;
 
 fn main() {
-    sycamore::render(|cx| {
-        view! {cx,
+    sycamore::render(|| {
+        view! {
             App{}
         }
     });
 }
 
 #[component]
-fn App<G: Html>(cx: Scope) -> View<G> {
-    let currency: &Signal<HashMap<String, Currency>> = create_signal(cx, HashMap::new());
-    let currency_vec:&Signal<Vec<String>> = create_signal(cx, Vec::new());
+fn App<G: Html>() -> View<G> {
+    let currency: Signal<HashMap<String, Currency>> = create_signal( HashMap::new());
+    let currency_vec:Signal<Vec<String>> = create_signal( Vec::new());
 
-    let price_nearby:&Signal<f64>=create_signal(cx, 0.0);
-    let price_far:&Signal<f64>=create_signal(cx, 0.0);
+    let price_nearby:Signal<f64>=create_signal( 0.0);
+    let price_far:Signal<f64>=create_signal( 0.0);
 
-    spawn_local_scoped(cx, async move {
+    spawn_local_scoped( async move {
         fetch("https://www.floatrates.com/daily/eur.json", |response| {
             if let Ok(devs) = serde_json::from_str::<HashMap<String, Currency>>(&response) {
                 let vec=devs.clone().into_keys().collect();
@@ -35,16 +35,17 @@ fn App<G: Html>(cx: Scope) -> View<G> {
         .await;
     });
 
-    view! { cx,
+    view! { 
         header{}
         main{
             article{
                 ul {
-                    Indexed(
-                        iterable=currency_vec,
-                        view=|cx, x| view! { cx,
+                    Keyed(
+                        iterable=*currency_vec,
+                        view=| x| view! { 
                             li { (x) }
                         },
+                        key=|x| x.clone(),
                     )
                 }
             }
