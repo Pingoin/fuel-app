88d0486bd55ed735ac9b864a7a5efc511ad33679
Pingoin <p.drente@gmx.de>
2023-11-04 20:38:00 +0100

    feat: added calculations

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use serde::{ Deserialize, Serialize};
 use sycamore::futures::spawn_local_scoped;
 use sycamore::prelude::*;
 
-use crate::utils::{fetch, set_stored_text, create_stored_signal};
+use crate::utils::{fetch, create_stored_signal};
 mod utils;
 
 fn main() {
@@ -26,6 +26,10 @@ fn App<G: Html>() -> View<G> {
         create_stored_signal(String::from("currency_nearby"), String::from("eur"));
     let price_far = create_stored_signal(String::from("price_far"), 5.779f64);
     let currency_far = create_stored_signal(String::from("currency_far"), String::from("pln"));
+
+    let fuel_usage=create_stored_signal(String::from("fuel_usage"), 5.0f64);
+    let fueling_detour_km=create_stored_signal(String::from("fueling_detour_km"), 50.0f64);
+    let fuel_amount=create_stored_signal(String::from("fuel amount"), 45.0f64);
 
     let conversion_factor = create_memo(move || {
         let near_string = currency_nearby.with(|cur| cur.clone());
@@ -51,11 +55,27 @@ fn App<G: Html>() -> View<G> {
 
     let price_far_converted = create_memo(move || price_far.get() * conversion_factor.get());
 
+    let fuel_kosts_near=create_memo(move || {
+        price_nearby.get()*fuel_amount.get()
+    });
+
+    let fuel_kosts_far=create_memo(move || {
+        price_far_converted.get()*fuel_amount.get()
+    });
+
+    let detour_kosts=create_memo(move || {
+        price_far_converted.get()*fuel_usage.get()*fueling_detour_km.get()/100.0
+    });
+
+    let savings=create_memo(move || {
+       fuel_kosts_near.get()-fuel_kosts_far.get()-detour_kosts.get()
+    });
+
+    currency.track();
     spawn_local_scoped(async move {
         fetch("https://www.floatrates.com/daily/eur.json", |response| {
             if let Ok(devs) = serde_json::from_str::<HashMap<String, Currency>>(&response) {
                 currency.set(devs);
-                set_stored_text("currencies", response)
             }
         })
         .await;
@@ -68,6 +88,7 @@ fn App<G: Html>() -> View<G> {
                 span{"Fuel price Nearby"}
                 div{
                     input(bind:valueAsNumber=price_nearby, type="number", min="0", step="0.01")
+                    " "
                     select(bind:value=currency_nearby){
                         CurrencyOptions{}
                     }
@@ -75,6 +96,7 @@ fn App<G: Html>() -> View<G> {
                 span{"Fuel price far"}
                 div{
                     input(bind:valueAsNumber=price_far, type="number", min="0", step="0.01")
+                    " "
                     select(bind:value=currency_far){
                         CurrencyOptions{}
                     }
@@ -90,7 +112,41 @@ fn App<G: Html>() -> View<G> {
                 } else {
                     view! { } // Now you don't
                 })
-
+                span{" Fuel usage"}
+                div{
+                    input(bind:valueAsNumber=fuel_usage, type="number", min="0", step="0.1")
+                    "l/100 km"
+                }
+                span{"Detour to cheaper fuelstation"}
+                div{
+                    input(bind:valueAsNumber=fueling_detour_km, type="number", min="0", step="1")
+                    " km"
+                }
+                span{"fuel amount"}
+                div{
+                    input(bind:valueAsNumber=fuel_amount, type="number", min="0", step="1")
+                    " l"
+                }
+                span{"fuel kosts near"}
+                div{
+                    ((fuel_kosts_near.get()*100.0).round() / 100.0)
+                    " €"
+                }
+                span{"fuel kosts far"}
+                div{
+                    ((fuel_kosts_far.get()*100.0).round() / 100.0)
+                    " €"
+                }
+                span{"detour kosts far"}
+                div{
+                    ((detour_kosts.get()*100.0).round() / 100.0)
+                    " €"
+                }
+                span{"savings"}
+                div{
+                    ((savings.get()*100.0).round() / 100.0)
+                    " €"
+                }
 
             }
         }
